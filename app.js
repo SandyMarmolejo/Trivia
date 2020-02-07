c01a4ca53ddb74675b619e34d1631e8cc71759eb
Sandy Marmolejo <sandymarmolejoc@gmail.com>
2020-02-07 17:04:17 -0500

    Modificación de ficheros

--- app.js
+++ app.js
@@ -1,177 +1,114 @@
-var resultado = 0;
-
 function aceptarNombre() {
+    let nombre = document.getElementById("txtNombre").value;
 
-    var name = document.getElementById("txtName").value;
-
-    if (name == "") {
-        alert("Ingrese su nombre");
+    if (nombre == "") {
+        alert("Ingresa tu nombre");
     } else {
-        document.getElementById("lblSaludaNombre").innerHTML = `Hola ${name}`;
+        document.getElementById("lblSaludaNombre").innerHTML = `Hola ${nombre}`;
+
+        document.getElementById("divTrivia").className = "ocultarDiv";
         document.getElementById("divSeleccioneTema").className = "mostrarDiv";
-        document.getElementById("divIngreseNombre").className = "ocultarDiv";
     }
 }
 
-function SeleccioneTema1() {
-    document.getElementById("divCulturaPregunta2").className = "ocultarDiv";
-    document.getElementById("divCulturaPregunta3").className = "ocultarDiv";
-    document.getElementById("divCulturaPregunta1").className = "mostrarDiv";
+function selectTema1() {
     document.getElementById("divSeleccioneTema").className = "ocultarDiv";
-    document.getElementById("divCultura").className = "mostrarDiv";
+    document.getElementById("divMusica").className = "ocultarDiv";
+    document.getElementById("divCulturaGeneral").className = "mostrarDiv";
+    document.getElementById("divPregunta1").className = "mostrarDiv";
+    document.getElementById("lblResultado").innerHTML = 0;
 }
 
-function RespondiendoPregunta1() {
-    document.getElementById("divCulturaPregunta1").className = "ocultarDiv";
-    document.getElementById("divCulturaPregunta3").className = "ocultarDiv";
-    document.getElementById("divIngreseNombre").className = "ocultarDiv";
+function selectTema2() {
     document.getElementById("divSeleccioneTema").className = "ocultarDiv";
-    document.getElementById("divCulturaPregunta2").className = "mostrarDiv";
-    document.getElementById("divCultura").className = "mostrarDiv";
+    document.getElementById("divCulturaGeneral").className = "ocultarDiv";
+    document.getElementById("divMusica").className = "mostrarDiv";
+    document.getElementById("divPregunta4").className = "mostrarDiv";
+    document.getElementById("lblResultado").innerHTML = 0;
+}
 
-    let alternativas = document.getElementsByName('rdbPregunta1');
 
-    for (let i = 0; i < alternativas.length; i++) {
-        if (alternativas[i].checked) {
-            let respuesta = alternativas[i].value;
+function responderPregunta1(respuesta) {
+    let resultado = parseInt(document.getElementById("lblResultado").innerHTML);
 
-            if (respuesta == "Canada") {
-                resultado = resultado + 10;
-            }
-        }
+    if (respuesta == "Antártida") {
+        resultado += 20;
     }
+
+    document.getElementById("lblResultado").innerHTML = resultado;
+
+    document.getElementById("divPregunta1").className = "ocultarDiv";
+    document.getElementById("divPregunta2").className = "mostrarDiv";
 }
 
-function RespondiendoPregunta2() {
-    document.getElementById("divIngreseNombre").className = "ocultarDiv";
-    document.getElementById("divSeleccioneTema").className = "ocultarDiv";
-    document.getElementById("divCulturaPregunta1").className = "ocultarDiv";
-    document.getElementById("divCulturaPregunta2").className = "ocultarDiv";
-    document.getElementById("divCulturaPregunta3").className = "mostrarDiv";
+function responderPregunta2(respuesta) {
+    let resultado = parseInt(document.getElementById("lblResultado").innerHTML);
 
-    let alternativas = document.getElementsByName('rdbPregunta2');
-
-    for (let i = 0; i < alternativas.length; i++) {
-        if (alternativas[i].checked) {
-            let respuesta = alternativas[i].value;
-
-            if (respuesta == "Amazonas") {
-                resultado = resultado + 10;
-            }
-        }
+    if (respuesta == "Amazonas") {
+        resultado += 20;
     }
+
+    document.getElementById("lblResultado").innerHTML = resultado;
+
+    document.getElementById("divPregunta2").className = "ocultarDiv";
+    document.getElementById("divPregunta3").className = "mostrarDiv";
 }
 
-function RespondiendoPregunta3() {
-    document.getElementById("divIngreseNombre").className = "ocultarDiv";
-    document.getElementById("divCulturaPregunta1").className = "ocultarDiv";
-    document.getElementById("divCulturaPregunta2").className = "ocultarDiv";
-    document.getElementById("divSeleccioneTema").className = "ocultarDiv";
-    document.getElementById("divCulturaPregunta3").className = "ocultarDiv";
-    document.getElementById("divCultura").className = "ocultarDiv";
+function responderPregunta3(respuesta) {
+    let resultado = parseInt(document.getElementById("lblResultado").innerHTML);
+
+    if (respuesta == "Tierra") {
+        resultado += 20;
+    }
+
+    document.getElementById("lblResultado").innerHTML = resultado;
+
+    document.getElementById("divPregunta3").className = "ocultarDiv";
     document.getElementById("divResultado").className = "mostrarDiv";
-
-    let alternativas = document.getElementsByName('rdbPregunta3');
-
-    for (let i = 0; i < alternativas.length; i++) {
-        if (alternativas[i].checked) {
-            let respuesta = alternativas[i].value;
-
-            if (respuesta == "Tierra") {
-                resultado = resultado + 10;
-            }
-        }
-    }
-
-    document.getElementById("lblResultado").innerHTML = "Tu resultado es " + resultado;
-
+    document.getElementById("divCulturaGeneral").className = "ocultarDiv";
 }
 
-function VolverAlMenu() {
+function responderPregunta4(respuesta) {
+    let resultado = parseInt(document.getElementById("lblResultado").innerHTML);
+
+    if (respuesta == "Whitney Houston") {
+        resultado += 20;
+    }
+
+    document.getElementById("lblResultado").innerHTML = resultado;
+
+    document.getElementById("divPregunta4").className = "ocultarDiv";
+    document.getElementById("divPregunta5").className = "mostrarDiv";
+}
+
+function responderPregunta5(respuesta) {
+    let resultado = parseInt(document.getElementById("lblResultado").innerHTML);
+
+    if (respuesta == "Céline Dion") {
+        resultado += 20;
+    }
+
+    document.getElementById("lblResultado").innerHTML = resultado;
+
+    document.getElementById("divPregunta5").className = "ocultarDiv";
+    document.getElementById("divPregunta6").className = "mostrarDiv";
+}
+
+function responderPregunta6(respuesta) {
+    let resultado = parseInt(document.getElementById("lblResultado").innerHTML);
+
+    if (respuesta == "Adele") {
+        resultado += 20;
+    }
+
+    document.getElementById("lblResultado").innerHTML = resultado;
+
+    document.getElementById("divPregunta6").className = "ocultarDiv";
+    document.getElementById("divResultado").className = "mostrarDiv";
+    document.getElementById("divMusica").className = "ocultarDiv";
+}
+
+function volverMenuPrincipal() {
+    document.getElementById("divResultado").className = "ocultarDiv";
     document.getElementById("divSeleccioneTema").className = "mostrarDiv";
-    document.getElementById("divCultura").className = "ocultarDiv";
-    document.getElementById("divMusica").className = "ocultarDiv";
-    document.getElementById("divResultado").className = "ocultarDiv";
-    document.getElementById("divIngreseNombre").className = "ocultarDiv";
-    document.getElementById("lblSaludaNombre").className = "ocultarDiv";
-
-    resultado = 0;
-}
-
-function SeleccioneTema2() {
-    document.getElementById("divIngreseNombre").className = "ocultarDiv";
-    document.getElementById("divSeleccioneTema").className = "ocultarDiv";
-    document.getElementById("divCultura").className = "ocultarDiv";
-    document.getElementById("divMusicaPregunta2").className = "ocultarDiv";
-    document.getElementById("divMusicaPregunta3").className = "ocultarDiv";
-    document.getElementById("divMusica").className = "mostrarDiv";
-    document.getElementById("divMusicaPregunta1").className = "mostrarDiv";
-}
-
-function RespondiendoPregunta4() {
-    document.getElementById("divIngreseNombre").className = "ocultarDiv";
-    document.getElementById("divSeleccioneTema").className = "ocultarDiv";
-    document.getElementById("divCultura").className = "ocultarDiv";
-    document.getElementById("divMusicaPregunta1").className = "ocultarDiv";
-    document.getElementById("divMusicaPregunta3").className = "ocultarDiv";
-    document.getElementById("divMusica").className = "mostrarDiv";
-    document.getElementById("divResultado").className = "ocultarDiv";
-    document.getElementById("divMusicaPregunta2").className = "mostrarDiv";
-
-    let alternativas = document.getElementsByName('rdbPregunta4');
-
-    for (let i = 0; i < alternativas.length; i++) {
-        if (alternativas[i].checked) {
-            let respuesta = alternativas[i].value;
-
-            if (respuesta == "DaddyYankee") {
-                resultado = resultado + 10;
-            }
-        }
-    }
-}
-
-function RespondiendoPregunta5() {
-    document.getElementById("divIngreseNombre").className = "ocultarDiv";
-    document.getElementById("divSeleccioneTema").className = "ocultarDiv";
-    document.getElementById("divCultura").className = "ocultarDiv";
-    document.getElementById("divMusicaPregunta1").className = "ocultarDiv";
-    document.getElementById("divMusicaPregunta3").className = "mostrarDiv";
-    document.getElementById("divMusica").className = "mostrarDiv";
-    document.getElementById("divResultado").className = "ocultarDiv";
-    document.getElementById("divMusicaPregunta2").className = "ocultarDiv";
-
-    let alternativas = document.getElementsByName('rdbPregunta5');
-
-    for (let i = 0; i < alternativas.length; i++) {
-        if (alternativas[i].checked) {
-            let respuesta = alternativas[i].value;
-
-            if (respuesta == "LaFaldita") {
-                resultado = resultado + 10;
-            }
-        }
-    }
-}
-
-function RespondiendoPregunta6() {
-    document.getElementById("divIngreseNombre").className = "ocultarDiv";
-    document.getElementById("divSeleccioneTema").className = "ocultarDiv";
-    document.getElementById("divCultura").className = "ocultarDiv";
-    document.getElementById("divMusica").className = "ocultarDiv";
-    document.getElementById("divResultado").className = "mostrarDiv";
-
-    let alternativas = document.getElementsByName('rdbPregunta6');
-
-    for (let i = 0; i < alternativas.length; i++) {
-        if (alternativas[i].checked) {
-            let respuesta = alternativas[i].value;
-
-            if (respuesta == "Reguetone") {
-                resultado = resultado + 10;
-            }
-        }
-    }
-
-    document.getElementById("lblResultado").innerHTML = "Tu resultado es " + resultado;
 }
